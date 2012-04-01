72ed35a3783b6ae9f3d0053a4d4fbb4b2bcf8235
MikeBarker_cp <SND\MikeBarker_cp>
2012-04-01 23:20:19 +0000

    remove deadlock from within CacheBase.DropData

--- src/Plethora.Cache/IArgument.cs
+++ src/Plethora.Cache/IArgument.cs
@@ -4,6 +4,8 @@ namespace Plethora.Cache
 {
     public interface IArgument<TData, TArgument>
     {
+        //TODO: Type documentation
+
         bool IsOverlapped(
             TArgument B,
             out IEnumerable<TArgument> notInB);
