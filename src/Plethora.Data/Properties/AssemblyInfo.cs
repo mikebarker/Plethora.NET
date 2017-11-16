1499e4729b165d92652c9e9cc597dc5ab4b8d417
Mike <mikey.barker@gmail.com>
2017-11-16 19:40:04 +0000

    Move the Data components to their own assembly.

--- src/Plethora.Data/Properties/AssemblyInfo.cs
+++ src/Plethora.Data/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Plethora.Finance")]
+[assembly: AssemblyTitle("Plethora.Data")]
 [assembly: AssemblyDescription("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e465d036-f4bd-404c-b674-92dee2c2c87d")] 
+[assembly: Guid("c7e9bfc1-8acc-4423-b0dc-a696bbb10da6")] 
